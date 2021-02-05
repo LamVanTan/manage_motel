5b9ae12f87426c2c6ee85a9f42958ed4f409319e
lamvantan <vantan291@yahoo.com>
2021-02-05 15:59:56 +0700

    update

---
+++ database/migrations/2021_02_05_082319_create__user_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('User', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->integer('permission');
+            $table->dateTimeTz('ngayBatDau');
+            $table->dateTimeTz('ngayKetThuc');
+            $table->integer('priceApp');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('User');
+    }
+}
